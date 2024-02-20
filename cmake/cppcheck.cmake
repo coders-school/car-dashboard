d9f0b99602de51a3b24a6536418937ed14f14e54
llisnikowski <l.lisnikowski@gmail.com>
2024-02-20 20:23:55 +0100

    Add cppcheck target for cmake

---
+++ cmake/cppcheck.cmake
@@ -0,0 +1,26 @@
+find_program(CPPCHECK cppcheck)
+
+if(NOT CPPCHECK)
+    message(SEND_ERROR "cppcheck was not found, check target is inaccessible")
+else()
+    message(STATUS "cppcheck was found, 'cppcheck-check' target can be used")
+    cmake_host_system_information(RESULT N_PROC QUERY NUMBER_OF_LOGICAL_CORES)
+    
+    add_custom_target(cppcheck-check COMMAND ${CPPCHECK} 
+        --cppcheck-build-dir=${CMAKE_BINARY_DIR}/cppcheck
+        --enable=all
+        --error-exitcode=1
+        --inconclusive                              # False positive can happen
+        --language=c++
+        --platform=native
+        --library=qt
+        -DQ_IMPORT_QML_PLUGIN
+        --project=${CMAKE_BINARY_DIR}/compile_commands.json
+        -i ${CMAKE_BINARY_DIR}                      # Exclude build from analysis
+        -j ${N_PROC}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        USES_TERMINAL
+        COMMENT "Checking code with Cppcheck"
+    )
+
+endif()
