fa662a3a30dd1ded91521a4f6bfc5a74207f11b5
llisnikowski <l.lisnikowski@gmail.com>
2024-03-03 14:55:29 +0100

    Add clang-tidy target

---
+++ cmake/clang-tidy.cmake
@@ -0,0 +1,30 @@
+find_program(CLANG_TIDY clang-tidy)
+
+if(NOT CLANG_TIDY)
+    message(SEND_ERROR "clang-tidy was not found, check and fix targets are inaccessible")
+else()
+    message(STATUS "clang-tidy was found, 'clang-tidy-check' and 'clang-tidy-fix' targets can be used")
+
+    add_custom_target(clang-tidy-check COMMAND ${CLANG_TIDY} 
+        ${ALL_SOURCES} 
+        -p=${CMAKE_CURRENT_BINARY_DIR} 
+        -use-color
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        USES_TERMINAL
+        COMMENT "Checking code with Clang-Tidy"
+    )
+
+    add_custom_target(clang-tidy-fix COMMAND ${CLANG_TIDY} 
+        ${ALL_SOURCES} 
+        -p=${CMAKE_CURRENT_BINARY_DIR} 
+        -use-color 
+        -fix-errors
+        -format-style=file
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        USES_TERMINAL
+        COMMENT "Checking code and applying automatic fixes with Clang-Tidy"
+    )
+
+endif()
+
+
