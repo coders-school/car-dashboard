a8d3083ca84e055b5c58892d8c16c7ec7ea27591
llisnikowski <l.lisnikowski@gmail.com>
2024-06-04 14:43:03 +0200

    Add precommit checking in CMake

---
+++ cmake/pre-commit-check.cmake
@@ -0,0 +1,14 @@
+find_program(PRECOMMIT pre-commit)
+
+if(NOT PRECOMMIT)
+    message(AUTHOR_WARNING "pre-commit was not found")
+else()
+    message(STATUS "pre-commit was found")
+
+    if(EXISTS ${CMAKE_SOURCE_DIR}/.git/hooks/pre-commit)
+        message(STATUS "pre-commit is set up")
+    else()
+        message(AUTHOR_WARNING "pre-commit is NOT set up. Please run \"pre-commit install\"")
+    endif()
+
+endif()
